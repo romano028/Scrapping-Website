c236dab2b497f608b9b4b736fa48800fc17c236c
so0me <inigo.iformatlogic@gmail.com>
2024-08-28 16:53:22 +0800

    Added .gitignore and README

--- server/server.js
+++ server/server.js
@@ -1,28 +1,14 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const cors = require('cors'); // Import cors
+const http = require('http');
 
-const app = express();
+const hostname = 'localhost';
 const port = 3000;
 
-app.use(cors()); // Enable CORS for all routes
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.post('/scrape', async (req, res) => {
-    try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://stockx.com/');
-        const title = await page.title();
-        await browser.close();
-
-        res.json({ success: true, title });
-    } catch (error) {
-        res.json({ success: false, message: error.message });
-    }
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end('Hello, World!\n');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
