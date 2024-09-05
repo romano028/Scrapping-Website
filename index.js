d248cc736e3bb531fa8cea851c37edec2e041dba
romano Mojica ednalan <romanoednalan227@gmail.com>
2024-09-05 09:49:12 +0800

    update

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
+  // scrapeLogic(res);
+  res.send("Render Puppeteer ");
 });
 
 app.get("/", (req, res) => {
