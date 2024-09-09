a969cffc12e29bdf71800a15ad777b3078a74d12
romano028 <romanoednalan227@gmail.com>
2024-09-09 16:57:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,71 +17,10 @@ app.use(cors({
     credentials: true
 }));
  
-// app.post("/scrape",urlencodedParser , (req, res) => {
-//   scrapeLogic(req , res);
-//   // res.send("Render Puppeteer ");
-// });
-app.post('/scrape', urlencodedParser, async (req, res) => {
-    const isValidURL = (string) => {
-        try {
-            new URL(string);
-            return true;
-        } catch (_) {
-            return false;
-        }
-    }
-    
-    const url = req.body.url;
-
-    if (!url || !isValidURL(url)) {
-        return res.status(400).json({error: 'Invalid URL'});
-    }
-
-    try {
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        });
-        const page = await browser.newPage();
-
-        await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36');
-        await page.evaluateOnNewDocument(() => {
-            Intl.DateTimeFormat().resolvedOptions().timeZone = 'Asia/Manila';
-        });
-
-        await page.setRequestInterception(true);
-        page.on('request', (request) => {
-            request.continue();
-        });
-
-        await page.goto(url, {
-            timeout: 50000,
-            waitUntil: 'networkidle2',
-        });
-
-        await page.evaluate(() => {
-            window.scrollTo(0, document.body.scrollHeight);
-        });
-
-        await page.evaluate(async () => {
-            const images = Array.from(document.querySelectorAll('img'));
-            await Promise.all(images.map(img => {
-                if (img.complete) return;
-                return new Promise((resolve) => {
-                    img.onload = resolve;
-                    img.onerror = resolve;
-                });
-            }));
-        });
-
-        const htmlContent = await page.content();
-        res.status(200).json([htmlContent]);
-    } catch (error) {
-        console.error('Server Error:', error); // Detailed server log
-        res.status(500).json({ error: 'An error occurred while scraping the page.' });
-    }
+app.post("/scrape",urlencodedParser , (req, res) => {
+  scrapeLogic(req , res);
+  // res.send("Render Puppeteer ");
 });
-
 
 app.post('/sheet', async (req, res) => {
     const auth = new google.auth.GoogleAuth({
